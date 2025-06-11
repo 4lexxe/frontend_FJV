2a426362b788bf856e15b21b9f7b1af2529d8df5
AlexUnju <alexxe@fi.unju.edu.ar>
2025-06-11 16:59:05 -0300

    feat(core): añadir componente de footer con redes sociales
    Implementar componente de pie de página que incluye:
    - Logo de la federación dentro de un círculo blanco para mejor visibilidad
    - Enlaces a redes sociales (Facebook, Instagram, Twitter, YouTube)
    - Menú de enlaces rápidos a las principales secciones
    - Información de contacto (dirección, correo electrónico, teléfono)
    - Formulario para suscripción a boletín informativo
    - Sección de derechos de autor con año actualizado dinámicamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: `
     <app-navbar></app-navbar>
     <main>
       <router-outlet></router-outlet>
     </main>
+    <app-footer></app-footer>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Federación Jujeña de Voleibol';
-  currentYear: number = new Date().getFullYear();
 }
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface SocialLink {
+  name: string;
+  url: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+
+  socialLinks: SocialLink[] = [
+    {
+      name: 'Facebook',
+      url: 'https://www.facebook.com/federacionjujenyavoley',
+      icon: 'fab fa-facebook-f'
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/federacionjujenyavoley',
+      icon: 'fab fa-instagram'
+    },
+    {
+      name: 'Twitter',
+      url: 'https://www.twitter.com/fjvoley',
+      icon: 'fab fa-twitter'
+    },
+    {
+      name: 'YouTube',
+      url: 'https://www.youtube.com/channel/federacionjujenyavoley',
+      icon: 'fab fa-youtube'
+    }
+  ];
+
+  footerLinks = [
+    { label: 'Inicio', url: '/' },
+    { label: 'Quiénes somos', url: '/quienes-somos' },
+    { label: 'Torneos', url: '/torneos' },
+    { label: 'Noticias', url: '/noticias' },
+    { label: 'Calendario', url: '/calendario' },
+    { label: 'Contacto', url: '/contacto' }
+  ];
+}
